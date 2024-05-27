be001a08227ec8a23ddc2641a3b21c3410fe5ea3
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-05-26 20:11:44 -0700

    Updated index.js to improve the content side of the webpage. Now, the footer is available with the 'add task' button at the bottom of the page.

--- src/index.js
+++ src/index.js
@@ -4,26 +4,75 @@ const main = document.getElementById('main');
 
 
 /*
+    Create nav content function
+*/
+function addNavGroup(name, nav) {
+    let group = document.createElement('h4');
+    group.classList.add('nav-content');
+    group.innerText = name;
+
+    group.addEventListener('click', () => {
+        // This event listener will filter the tasks by their appropriate grouping
+        console.log(`The name of this group is ${name}`);
+    });
+
+    nav.appendChild(group);
+}
+
+/*
     Single Responsibility - instead of loading the entire page, load the nav independent
     to the content on the right side.
 */
+
+/*
+    Nav Loader
+*/
 function navLoader() {
     let navDiv = document.createElement('div');
-    navDiv.classList.add('flex', 'single', 'border', 'height', 'width');
+    navDiv.classList.add('flex', 'column', 'center-content',
+        'single', 'border');
+
+    addNavGroup('Today', navDiv);
+    addNavGroup('Tomorrow', navDiv);
+    addNavGroup('This Week', navDiv);
+    addNavGroup('This Month', navDiv);
 
     return navDiv;
 }
 
+/*
+    Content Loader
+*/
 function contentLoader() {
-    let contentDiv = document.createElement('div');
-    contentDiv.classList.add('flex', 'double', 'border', 'height', 'width');
+    let wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add('flex', 'column', 'double', 'border');
 
-    return contentDiv;
+    let contentDiv = document.createElement('div');
+    contentDiv.classList.add('flex', 'height', 'border');
+    wrapperDiv.appendChild(contentDiv);
+
+    let footerDiv = document.createElement('div');
+    footerDiv.classList.add('flex', 'center-content', 'border');
+    footerDiv.id = "task-footer";
+    wrapperDiv.appendChild(footerDiv);
+
+    let addTaskButton = document.createElement('button');
+    addTaskButton.innerText = "Add Task";
+    addTaskButton.addEventListener('click', () => {
+        //This event listener will be used to add tasks
+        console.log("The add task button works!");
+    });
+    footerDiv.appendChild(addTaskButton);
+
+    return wrapperDiv;
 }
 
+/*
+    Home Page Loader
+*/
 function homePageLoader() {
     let contentDiv = document.createElement('div');
-    contentDiv.classList.add('flex');
+    contentDiv.classList.add('flex', 'height', 'width');
     let nav = navLoader();
     let content = contentLoader();
 
